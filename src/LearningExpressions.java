e282e58d9e347d4af323dd2fa1fa2b1074298fd1
gabsgonn <gab.gd23@gmail.com>
2024-11-15 23:51:16 -0300

    add `tasks`

---
+++ src/LearningExpressions.java
@@ -0,0 +1,16 @@
+public class LearningExpressions {
+
+    public static void main (String[] args) {
+
+        // expressions = operands & operators
+        // operands = values, variables, numbers, quantity
+        // operators = + - * / %
+
+        double friends = 10;
+
+        friends = (double) friends / 2;
+
+        System.out.println(friends);
+
+    }
+}
---
+++ src/LearningGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class LearningGUI {
+
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You are "+age+" years old :D.");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter you height"));
+        JOptionPane.showMessageDialog(null, "You are "+height+" cm tall :O.");
+
+    }
+}
